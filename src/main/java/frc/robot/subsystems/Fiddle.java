3b754c8222fa0cb50f54b12a05d50ab8a51b3014
indoorfish01 <broland607@gmail.com>
2024-01-11 19:08:22 -0500

    wii song commit (radio still broken)

---
+++ src/main/java/frc/robot/subsystems/Fiddle.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.Orchestra;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Fiddle extends SubsystemBase {
+
+  private final TalonFX fiddle;
+  private final Orchestra orchestra;
+
+  /** Creates a new Fiddle. */
+  public Fiddle() {
+
+    fiddle = new TalonFX( Constants.FiddleConstants.fiddleID );
+    fiddle.setNeutralMode(NeutralModeValue.Brake);
+    fiddle.setInverted(false);
+
+    orchestra = new Orchestra();
+    orchestra.addInstrument(fiddle);
+    orchestra.loadMusic(Constants.FiddleConstants.defaultSong);
+  }
+
+  @Override
+  public void periodic() {
+
+  }
+
+  public void playFiddle() {
+    orchestra.play();
+  }
+
+  public void pauseFiddle() {
+    orchestra.pause();
+  }
+
+  public void stopFiddle() {
+    orchestra.stop();
+  }
+
+  public boolean isPlayingFiddle() {
+    return orchestra.isPlaying();
+  }
+
+  public void loadMusic(String filepath) {
+    orchestra.loadMusic(filepath);
+  }
+}
